8affd501508896e83e79c57de1890886e6386bad
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-06-25 23:36:21 -0400

    First Commit W9 Deliverables

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+mongoose.connect(process.env.URL)
+mongoose.connection.on("open", ()=> {console.log("connected to mongoose");})
+
+module.exports=mongoose
