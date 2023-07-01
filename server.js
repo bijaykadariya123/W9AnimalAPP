540299fcfe8993a7f84c68ce1461505ad7b31d75
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-07-01 09:54:12 -0400

    controller update

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 require("dotenv").config()
 const express = require("express")
-const morgan = require("morgan")
+// const morgan = require("morgan")
 const animalRoutes = require("./controllers/animalRoutes.js")
 const app = express()
 app.use(animalRoutes)
 
 
 
-// app.get("/", (req, res)=>{
-//     res.send("Hello World")
-// })
-app.use(mogran("dev"))
-// app.use("/", animalRoutes)
 
 
 
