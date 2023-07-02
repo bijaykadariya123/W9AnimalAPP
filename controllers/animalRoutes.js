c34b39d51485de856196793b2a89e90e2911ae33
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-07-01 23:06:06 -0400

    upated code

--- controllers/animalRoutes.js
+++ controllers/animalRoutes.js
@@ -3,12 +3,61 @@ const AnimalModel = require("../models/animal.js")
 
 const router = express.Router()
 
+//INDEX
 router.get("/", async (req, res) => {
    const allAnimals = await AnimalModel.find({})
    res.render("index.ejs",{allAnimals}) 
 })
+
+// NEW
 router.get("/new", (req, res)=>{
-    res.render("animal/new.ejs")
+    res.render("new.ejs")
 } )
+
+////////////////delete
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    const oneAnimal = await AnimalModel.findByIdAndDelete(id, req.body);
+    res.redirect('/animal', {oneAnimal, id})
+})
+
+//update
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    req.body.extinct = req.body.extinct === 'on' ? true : false;
+    const oneAnimal = await AnimalModel.findByIdAndUpdate(id, req.body);
+    res.redirect('/animal', {oneAnimal, id})
+})
+
+
+//////////////////////create
+router.post('/', async (req, res) => {
+    if(req.body.extinct === 'on'){
+        req.body.extinct = true;
+    }else {
+        req.body.readyToEat = false;
+    }
+   const addAnimal= await AnimalModel.create(req.body);
+    res.redirect('/animal', {addAnimal});
+})
+
+// Edit
+router.get('/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const oneAnimal = await AnimalModel.findById(id);
+    res.render('edit.ejs', {oneAnimal, id })
+})
+
+
+////////////////////show
+router.get("/:id", async (req, res)=>{
+    const id = req.params.id
+    const oneAnimal =  await AnimalModel.findById(id)
+    res.render("show.ejs", {oneAnimal, id})
+})
+
+
+
+
 
 module.exports= router
--- server.js
+++ server.js
@@ -2,11 +2,14 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const animalRoutes = require("./controllers/animalRoutes.js")
+const methodOverride = require('method-override');
 const app = express()
 
+
 //middleware
-app.use(express.urlencoded())// allows the req.body to be read from the form
 app.use(morgan("dev"))
+app.use(express.urlencoded())// allows the req.body to be read from the form
+app.use(methodOverride("_method"))
 
 //Routes
 app.use("/animal", animalRoutes)
